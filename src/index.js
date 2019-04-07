46ee24e2fa10a6abdad3742e885a7ea3b5bb2ab0
Hugo Meireles <meireles.hmm@gmail.com>
2019-04-07 23:21:09 +0100

    create start kit for react app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <div>
+    <h1>Start Kit</h1>
+    <p>The start kit to build app in react</p>
+  </div>,
+  document.getElementById('app')
+);
